936b1879172368d35049e99b39aa3002bcbe7ffa
mawoo <hanjutun@gmail.com>
2025-01-21 16:36:44 +0900

    MyTodo 추가

---
+++ MyTodo/app/src/main/java/cokr/oneweeks/mytodo/data/Todo.kt
@@ -0,0 +1,13 @@
+package cokr.oneweeks.mytodo.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity()
+data class Todo (
+    @PrimaryKey(autoGenerate = true)
+    val num: Int = 0,
+    val title: String,
+    val completed: Boolean
+)
---
+++ MyTodo/app/src/main/java/cokr/oneweeks/mytodo/data/TodoDao.kt
@@ -0,0 +1,23 @@
+package cokr.oneweeks.mytodo.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface TodoDao {
+    //CRUD
+    @Insert
+    fun insert(todo: Todo)
+
+    @Query("select * from todo")
+    fun getAllTodos():List<Todo>
+
+    @Update
+    fun update(todo: Todo)
+
+    @Delete
+    fun delete(todo: Todo)
+}
